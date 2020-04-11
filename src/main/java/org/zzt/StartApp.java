9e96145a177b329bc409b3a066a0c35fb57735f7
Peace420 <zztwjc@aliyun.com>
2020-04-11 16:05:34 +0800

    测试

---
+++ src/main/java/org/zzt/StartApp.java
@@ -0,0 +1,18 @@
+package org.zzt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: StartApp
+ * date: 2020/3/29 21:10
+ * author: 朱忠涛
+ * version: 1.0
+ */
+@SpringBootApplication
+public class StartApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class);
+    }
+}
